7a209bbec335d4f41054b01f9f6aee23899fcc3f
root <1021130731@qq.com>
2023-02-20 17:31:34 +0800

    修改降噪算法,更改了显示结果

--- reductNoise.m
+++ reductNoise.m
@@ -1,8 +1,17 @@
-J = imread('dogNoise.bmp'); % 读入噪声图像
+clc;clear;
+
+filename = 'dog';
+originalFilename= strcat(filename,'.bmp');
+noiseFilename= strcat(filename,'Noise','.bmp') ;
+
+O = imread(originalFilename); % 读入噪声图像
+J = imread(noiseFilename); % 读入噪声图像
+subplot(151),imshow(O),title("原图像");
+subplot(152),imshow(J),title("噪声图像");
 
 f2=fftshift(fft2(J));
 f3=log(abs(f2));
-subplot(131),imshow(mat2gray(f3)),title("噪声图像频谱");
+subplot(153),imshow(mat2gray(f3)),title("噪声图像频谱");
 
 % 中值滤波
 filtered = medfilt2(J);
@@ -23,7 +32,7 @@ for u=1:m
 end
 G=ifftshift(G);
 g=uint8(real(ifft2(G)));
-subplot(132),imshow(g),title("去噪后图像");
+subplot(154),imshow(g),title("去噪后图像");
 g1=fftshift(fft2(double(g)));
 g2=log(abs(g1));
-subplot(133),imshow(mat2gray(g2)),title("去噪后频谱");
+subplot(155),imshow(mat2gray(g2)),title("去噪后频谱");
